bcb69480c358a6a21838e7cfa1b0425c6a07e694
John Walker <john@prodo.com>
2013-07-25 14:43:24 +0100

    Add second track

--- js/app.js
+++ js/app.js
@@ -1,25 +1,67 @@
 ﻿// Wait for Cordova to load
 //
-document.addEventListener("deviceready", onDeviceReady, false);
+//document.addEventListener("deviceready", onDeviceReady, false);
 
-// Cordova is ready
-//
-function onDeviceReady() {
-    playAudio("http://audio.ibeat.org/content/p1rj1s/p1rj1s_-_rockGuitar.mp3");
+var track1 = "http://audio.ibeat.org/content/p1rj1s/p1rj1s_-_rockGuitar.mp3";
+var track2 = "http://audio.ibeat.org/content/enoe/enoe_-_Student_Beat.mp3";
+
+function getTrackNumber(elm) {
+    return elm.parents('ul').data('track');
 }
+
+$("a[href*=#]").click(function (e) {
+    e.preventDefault();
+});  
+
+$(".start").click(function () {
+    var trackNum = getTrackNumber($(this));
+
+    if (trackNum === 1) {
+        playAudio(track1, 1);
+    }
+    else {
+        playAudio(track1, 2);
+    }
+});
+
+$(".stop").click(function () {
+    var trackNum = getTrackNumber($(this));
+    stopAudio(trackNum);
+});
+
+$(".volumeup").click(function () {
+    var trackNum = getTrackNumber($(this));
+    setVolume("0.8", trackNum);
+});
+
+
+$(".volumedown").click(function () {
+    setVolume("0.2", trackNum);
+});
+
+//function onDeviceReady() {
+//    playAudio("http://audio.ibeat.org/content/p1rj1s/p1rj1s_-_rockGuitar.mp3");
+//}
 
 // Audio player
 //
-var my_media = null;
+var my_media = {};
 
 // Play audio
 //
-function playAudio(src) {
+function playAudio(src, track) {
     // Create Media object from src
-    my_media = new Media(src, onSuccess, onError);
+    my_media[track] = new Media(src, onSuccess, onError);
 
     // Play audio
-    my_media.play();
+    my_media[track].play();
+}
+
+function stopAudio(track) {
+    if (my_media[track]) {
+        my_media[track].stop();
+    }
+    my_media[track] = null;
 }
 
 function onSuccess() {
@@ -35,8 +77,8 @@ function onError(error) {
 
 // Set audio volume
 //
-function setVolume(volume) {
-    if (my_media) {
-        my_media.setVolume(volume);
+function setVolume(volume, track) {
+    if (my_media[track]) {
+        my_media[track].setVolume(volume);
     }
 }
